7c56c953b1b6d230d9f19cdcb629878f45408b79
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-08 13:04:39 -0700

    creates PUT score with empty req.param

--- server.js
+++ server.js
@@ -34,9 +34,15 @@ app.get('/api/protips/:id', function (req, res) {
 });
 
 //UPDATE ONE PROTIP SCORE
-
-
-
+app.put('/api/protips/:id', function (req, res) {
+  db.ProTip.findOne({_id: req.params.id}, function(err, selectedProTip) {
+    selectedProTip.tipScore = req.???.tipScore
+    selectedCareer.save(function (err, savedUpdate) {
+      if (err) {return console.log(err);}
+      res.json(savedUpdate);
+    });
+  });
+});
 
 //JSON ENDPOINTS
 app.get('/api', function api_index(req, res) {
