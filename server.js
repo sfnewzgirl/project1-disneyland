a25c45bbf2a4db867eba400b9edc986a58401d98
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-08 11:38:39 -0700

    adds json endpoints

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
+//REQUIRE MODULES
 var express = require('express'),
     app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//REQUIRE DATABASE
 var db = require('./models');
+
+//ROUTES
+app.use(express.static('public'));
+
+
+app.get('/', function homepage(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+
+
+//JSON ENDPOINTS
+app.get('/api', function api_index(req, res) {
+  // TODO: Document all your api endpoints below
+  res.json({
+    ParkProTipsEndpoints: true,
+    message: "Welcome to Our Park Pro Tips API!",
+    documentationUrl: "https://github.com/sfnewzgirl/project1-disneyland/blob/master/README.md",
+    baseUrl: "https://parkprotips.herokuapp.com/",
+    endpoints: [
+      {method: "GET", path: "/api", description: "Homepage"},
+      {method: "GET", path: "/api/protips", description: "lists all protips"},
+      {method: "GET", path: "/api/protips/:id", description: "lists one protip"},
+      {method: "GET", path: "/api/protips/:id/tipScore", description: "lists one protip score"},
+      {method: "POST", path: "/api/protips", description: "adds one protip"},
+      {method: "PUT/PATCH", path: "/api/protips/:id", description: "updates one protip"},
+      {method: "DELETE", path: "api/protips/:id", description: "deletes one protips"}
+    ]
+  })
+});
+
+// LISTEN TO LOCALHOST
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is up and running on http://localhost:3000/');
+});
