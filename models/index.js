541ae07648d9fdd149cd3a4b8e7ed45aba83140f
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-08 12:23:32 -0700

    fixes index local host route

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost:3000");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost");
 
 module.exports.Ride = require("./ride.js");
 module.exports.ProTip = require("./proTip.js");
