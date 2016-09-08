6e7845b579c03190232846c2b3a3576b5156ba05
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-07 21:34:51 -0700

    adds Ride Schema to index.js

---
+++ models/index.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+//find correct connection??
+//this is from personal api
+//mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
+
+module.exports.Ride = require("./ride");
