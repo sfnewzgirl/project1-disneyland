cfdb03d3afb29e34fa38736faa021ec091e5c9af
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-08 12:15:03 -0700

    adds second file to seed.js

--- seed.js
+++ seed.js
@@ -1,24 +1,27 @@
 var db = require('./models');
 
-var proTipList =[];
-proTipList.push({
-                tipTitle: 'Get a Paint the Night On-the-Go Dining Package and VIP seating at the parade',
-                tipDescription: 'Several restaurants in the park offer on-the-go dining options that you reserve and pick up at a predetermined time. When you pick up your lunch you also get a VIP pass to a seating area at the start of the Paint The Night parade.',
-                tipScore: 5,
-                tipResource: true,
-                tipResourceInfo: 'https://disneyland.disney.go.com/dining/'
-                });
+var proTipList = [
+  {
+    tipTitle: 'Get a Paint the Night On-the-Go Dining Package and VIP seating at the parade',
+    tipDescription: 'Several restaurants in the park offer on-the-go dining options that you reserve and pick up at a predetermined time. When you pick up your lunch you also get a VIP pass to a seating area at the start of the Paint The Night parade.',
+    tipScore: 5,
+    tipResource: true,
+    tipResourceInfo: 'https://disneyland.disney.go.com/dining/'
+  },
+  {
+    tipTitle: 'Get a FastPass for Cars First',
+    tipDescription: 'The Cars ride in California Adventure has the longest wait times in the park. Arrive for the rope drop and head directly to get a FastPass, then return at your pass time',
+    tipScore: 11,
+    tipResource: false,
+    tipResourceInfo: ''
+  }
+];
 
-db.ProTrip.remove({}, function(err,protips) {
-  if (err) {
-    console.log('remove error', err);
-  } else {
-    console.log('removed all protips');
+db.ProTip.remove({}, function(err,protips) {
 
-    db.ProTrip.create(proTipList, function (err, protips) {
+    db.ProTip.create(proTipList, function (err, protips) {
       if (err) { return console.log('err', err); }
       console.log('created', protips.length, 'protips');
       process.exit();
     });
-  }
 });
