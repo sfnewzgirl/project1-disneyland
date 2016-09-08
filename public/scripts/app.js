6d869b35cac7cf47013dd0f7e6e92526653861c2
Andrew <andrew.vinocur@gmail.com>
2016-09-08 12:58:37 -0700

    adds logging of count variable to console

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,7 @@
 console.log ('app.js is linked!')
 
 function clicker(){
-  var count = parseInt($("~ .count", this).text());
-
+  var count = parseInt($("~ .count", this).text()); //this will be the variable that holds our count
   if($(this).hasClass("up")) {
     var count = count + 1;
 
@@ -11,14 +10,18 @@ function clicker(){
     var count = count - 1;
      $("~ .count", this).text(count);
   }
+    console.log(count);
 
-  $(this).parent().addClass("bump"); // animation on the upvote ever 400ms
+  $(this).parent().addClass("bump"); // animation on the upvote every 400ms
 
   setTimeout(function(){
     $(this).parent().removeClass("bump");
   }, 400);
 }
 
+
+
+///list of all of my listeners that will be active
 function init() {
   $(".increment").click(clicker);
 }
