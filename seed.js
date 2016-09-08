b19267150bbc83249a0474598760e8e48ec71a17
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-08 11:20:14 -0700

    creates proTip in seed.js

--- seed.js
+++ seed.js
@@ -1,4 +1,10 @@
-
-
-
 var db = require('./models');
+
+var proTipList =[];
+proTipList.push({
+                tipTitle: 'Get a Paint the Night On-the-Go Dining Package and VIP seating at the parade',
+                tipDescription: 'Several restaurants in the park offer on-the-go dining options that you reserve and pick up at a predetermined time. When you pick up your lunch you also get a VIP pass to a seating area at the start of the Paint The Night parade.',
+                tipScore: '5',
+                tipResource: 'true',
+                tipResourceInfo: 'https://disneyland.disney.go.com/dining/'
+                });
